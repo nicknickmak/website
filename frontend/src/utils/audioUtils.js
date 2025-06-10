8fbc674deaf0a8c6379ef2c4ed50b2e8c56277cf
nicknickmak <nicknickmak@gmail.com>
2025-06-10 18:53:54 -0400

    fix: error that shows on refresh for audio not allowed to play on refresh

--- frontend/src/utils/audioUtils.js
+++ frontend/src/utils/audioUtils.js
@@ -6,15 +6,20 @@
  * @param {number} [vol=0.5] - The volume at which to play the audio (range: 0.0 to 1.0).
  */
 export function playAudio(file, volume = 0.5){
-    var audio = document.createElement('audio');
-    audio.src = file;
-    audio.volume = volume;
-    document.body.appendChild(audio);
-    audio.play().catch(() => {
-        // Autoplay was prevented, ignore error
-    });
-    
-    audio.onended = function () {
-      this.parentNode.removeChild(this);
-    }
+  // This prevents error for autoplay being blocked by browser
+  // when the document is hidden (e.g., when the tab is not active).
+  if (document.hidden) {
+    return;
   }
+  var audio = document.createElement("audio");
+  audio.src = file;
+  audio.volume = volume;
+  document.body.appendChild(audio);
+  audio.play().catch(() => {
+    // Autoplay was prevented, ignore error
+  });
+
+  audio.onended = function () {
+    this.parentNode.removeChild(this);
+  };
+}
