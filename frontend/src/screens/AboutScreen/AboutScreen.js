1b0211fdf4198a8e77d067d30236899c1f8d4b8d
nicknickmak <nicknickmak@gmail.com>
2025-06-10 17:46:23 -0400

    feat: make about me sections responsive for mobile

--- frontend/src/screens/AboutScreen/AboutScreen.js
+++ frontend/src/screens/AboutScreen/AboutScreen.js
@@ -2,9 +2,7 @@ import "./AboutScreen.css";
 import "../tabs.css";
 
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import World_Music from "../../images/World_Music.jpeg";
-import Robot from "../../images/Robot.jpg";
-import Winners from "../../images/2019-Winner.jpg";
+
 import TabIcon from "../../images/TabIcon2.png";
 
 import tabHover from "../../audio/tabHover.mp3";
@@ -12,7 +10,8 @@ import tabClick from "../../audio/tabClick.mp3";
 
 import { useIsMobile } from "../../utils/useIsMobile";
 import { playAudio } from "../../utils/audioUtils";
-}
+
+import AboutSections from "./AboutSections";
 
 export default function AboutScreen() {
   const tabTitles = ["First Robotics", "University", "Career"];
@@ -45,107 +44,21 @@ export default function AboutScreen() {
           <TabPanel>
             <div className="scrollContent1">
               <div className="scrollContent2">
-                <img
-                  className="float-image-left"
-                  src={Robot}
-                  alt="Robot from Robotics Team I founded"
-                  width="300"
-                ></img>
-                <p>
-                  While my high school was rich in opportunity for the arts,
-                  there was an imbalance. All the emphasis for the arts meant a
-                  lack of focus for STEM education, a career path I was
-                  interested in. To supplement my passion for Engineering at the
-                  time, I decided to start the school's <i>FIRST</i> robotics
-                  team. Initially it was difficult to find support, because no
-                  one knew about the idea I had. But when the word spread, other
-                  students with similar interests came. The team started with
-                  just me attempting to fundraise the registration fee of
-                  $6,000. Luckily, with the aid of a grant I applied for and the
-                  school's administration, I was able to begin my endeavors.
-                  Building the{" "}
-                  <img
-                    className="float-image-right"
-                    src={Winners}
-                    alt="2019 Winners of Robotics"
-                    width="300"
-                  ></img>
-                  team's first year robot was tough. If it weren't for the help
-                  of the dedicated members and experienced parents, the team
-                  would not have had a functioning robot at all. While the robot
-                  did not do so well in competition the first year, it was an
-                  worthy accomplishment for the team nonetheless. In the
-                  following years, the team caught on successfully and expanded
-                  to 40+ members, becoming the largest organization in the
-                  school. From the founding team, I learned many skills from
-                  CAD, machining, Java robotics programming, electrical
-                  engineering, and business development. Currently,{" "}
-                  <a href="http://www.team5700.org/">
-                    {" "}
-                    Team 5700: SOTA Cyberdragons
-                  </a>{" "}
-                  is thriving in the world of <i>FIRST</i> Robotics, winning San
-                  Francisco Regional 2019, winning Chezy Champs 2019 and
-                  competing in the international <i>FIRST</i> championships in
-                  Houston, Texas.
-                </p>
+                <AboutSections section="FRC_Robotics" />
               </div>
             </div>
           </TabPanel>
           <TabPanel>
             <div className="scrollContent1">
               <div className="scrollContent2">
-                <h2 className="about-section">
-                  University of California, San Diego
-                </h2>
-
-                <p>
-                  At UC San Diego, I have taken a variety of courses that have
-                  deepened my understanding of computer science and engineering.
-                  Some of the most impactful classes include Data Structures &
-                  Algorithms, Computer Architecture, Software Engineering, and
-                  Artificial Intelligence. These courses have provided me with a
-                  strong foundation in both theoretical concepts and practical
-                  applications.
-                </p>
-                <p>
-                  Beyond academics, I am actively involved in several student
-                  organizations. I am a member of IEEE, ACM, and Triton XR,
-                  where I have participated in technical workshops, hackathons,
-                  and collaborative projects. These organizations have allowed
-                  me to connect with like-minded peers, develop leadership
-                  skills, and contribute to innovative projects on campus.
-                </p>
-                <img
-                  className="float-image-left"
-                  src={World_Music}
-                  alt="UCSD Organizations"
-                  width="300"
-                />
-                <p>
-                  My involvement in these organizations has been instrumental in
-                  shaping my university experience, providing opportunities to
-                  learn from others, work on interdisciplinary teams, and stay
-                  engaged with the latest advancements in technology.
-                </p>
+                <AboutSections section="University" />
               </div>
             </div>
           </TabPanel>
           <TabPanel>
             <div className="scrollContent1">
               <div className="scrollContent2">
-                <h2 className="about-section">Career</h2>
-                <p>
-                  With my second year of university, I decided to return to the
-                  world of STEM by joining engineering societies like IEEE, ACM,
-                  and Triton XR and applying to various staff positions to
-                  promote my involvement. In IEEE I became a project lead for
-                  it's new annual project, Robocup Soccer. In Triton XR, I
-                  became a project lead assistant so I could get more involved
-                  in the creation of virtual reality projects, a topic that has
-                  piqued my career interests.
-                </p>{" "}
-                <br></br>
+                <AboutSections section="Career" />
               </div>
             </div>
           </TabPanel>
---
+++ frontend/src/screens/AboutScreen/AboutSections.js
@@ -0,0 +1,156 @@
+import Robot from "../../images/Robot.jpg";
+import Winners from "../../images/2019-Winner.jpg";
+
+/**
+ * Renders different sections of the About page based on the provided section key.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {"FRC_Robotics"|"University"|"Career"} props.section - The section to display.
+ * @returns {JSX.Element} The rendered content for the specified section, or a message if the section is invalid.
+ */
+function AboutSections({ section }) {
+  switch (section) {
+    case "FRC_Robotics":
+      return (
+        <>
+          <h2 className="about-section">
+            <i>
+              <b>FIRST</b>
+            </i>{" "}
+            Robotics Team 5700
+          </h2>
+          <img
+            className="float-image-left"
+            src={Robot}
+            alt="Robot from Robotics Team I founded"
+            width="300"
+          ></img>
+          <p>
+            While my high school was rich in opportunity for the arts, there was
+            an imbalance. All the emphasis for the arts meant a lack of focus
+            and resources STEM education, a career path I was interested in. To
+            supplement my early passion for Engineering, I decided to start the
+            school's{" "}
+            <i>
+              <b>FIRST</b>
+            </i>{" "}
+            robotics team. Initially it was difficult to find support, because
+            no one knew about the idea I had. But when the word spread, other
+            students with similar interests came. The team started with just me
+            attempting to fundraise the registration fee of $6,000. Luckily,
+            with the aid of a grant I applied for and the school's
+            administration, I was able to begin my endeavors. Building the{" "}
+            <img
+              className="float-image-right"
+              src={Winners}
+              alt="2019 Winners of Robotics"
+              width="300"
+            ></img>
+            team's first year robot was tough. If it weren't for the help of the
+            dedicated members and experienced parents, the team would not have
+            had a functioning robot at all. While the robot did not do so well
+            in competition the first year, it was an worthy accomplishment for
+            the team nonetheless. In the following years, the team caught on
+            successfully and expanded to 40+ members, becoming the largest
+            organization in the school. From the founding team, I learned many
+            skills from CAD, machining, Java robotics programming, electrical
+            engineering, and business development. Currently,{" "}
+            <a href="http://www.team5700.org/"> Team 5700: SOTA Cyberdragons</a>{" "}
+            is thriving in the world of <i>FIRST</i> Robotics, winning San
+            Francisco Regional 2019, winning Chezy Champs 2019 and competing in
+            the international <i>FIRST</i> championships in Houston, Texas.
+          </p>
+        </>
+      );
+    case "University":
+      return (
+        <>
+          <h2 className="about-section">University Experience</h2>
+          <p>
+            I earned my Bachelor of Science in Computer Science from the
+            University of California, San Diego, graduating in January 2022. My
+            time at UCSD was marked by a rigorous academic curriculum, hands-on
+            projects, and active involvement in the campus tech community.
+          </p>
+          <h2 className="about-section">Relevant Coursework</h2>
+          <ul>
+            <li>
+              <b>Advanced Data Structures and Algorithms</b> – mastering
+              efficient problem-solving techniques
+            </li>
+            <li>
+              <b>Software Engineering</b> – learning best practices for
+              scalable, maintainable code
+            </li>
+            <li>
+              <b>Software System Design &amp; Implementation</b> – building
+              robust, real-world applications
+            </li>
+            <li>
+              <b>Computer Networks</b> – understanding protocols and distributed
+              systems
+            </li>
+            <li>
+              <b>Principles of Computer Operating Systems</b> – exploring OS
+              architecture and resource management
+            </li>
+            <li>
+              <b>Computer Graphics and Rendering</b> – creating visually
+              compelling digital content
+            </li>
+            <li>
+              <b>Computer Animation</b> – bringing interactive experiences to
+              life
+            </li>
+            <li>
+              <b>3D User Interaction</b> – designing intuitive interfaces for
+              immersive environments
+            </li>
+            <li>
+              <b>Virtual Reality / Augmented Reality Research</b> –
+              investigating cutting-edge XR technologies
+            </li>
+          </ul>
+          <h2 className="about-section">Extracurricular Activities</h2>
+          <ul>
+            <li>
+              <b>Triton XR</b>: Served as Lead Software Developer, where I
+              organized technical workshops and led the development of the
+              organization’s website, fostering a collaborative environment for
+              students passionate about extended reality.
+            </li>
+            <li>
+              <b>ACM UCSD</b>: Active member, participating in coding
+              competitions, hackathons, and collaborative software projects that
+              honed my teamwork and problem-solving skills.
+            </li>
+          </ul>
+        </>
+      );
+    case "Career":
+      return (
+        <>
+          <h2 className="about-section">Career</h2>
+          <p>
+            After graduating from UC San Diego with a degree in Computer
+            Science, I began my professional journey as a software engineer. My
+            career has spanned roles in both consumer and enterprise
+            applications, where I have contributed to the development of
+            scalable web applications, cloud-based solutions, and innovative
+            user experiences. I have experience working with React and Angular,
+            backend DotNet, MS SQL Server, and Microsoft Azure Cloud. My passion
+            for technology drives me to continuously learn and adapt, and I
+            enjoy collaborating with cross-functional teams to deliver impactful
+            products. Throughout my career, I have focused on writing clean,
+            maintainable code and mentoring junior developers to help them grow
+            in their roles.
+          </p>
+        </>
+      );
+    default:
+      return <div>TThis is not a valid section</div>;
+  }
+}
+
+export default AboutSections;
