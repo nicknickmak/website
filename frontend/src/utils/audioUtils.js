ef61e837c8c9040b0d291234ff5add0c37a1631e
nicknickmak <nicknickmak@gmail.com>
2025-06-10 23:19:57 -0400

    fix: revert play audio changes

--- frontend/src/utils/audioUtils.js
+++ frontend/src/utils/audioUtils.js
@@ -1,16 +1,10 @@
-
 /**
  * Plays an audio file.
  *
  * @param {string} file - The path or URL to the audio file to play.
- * @param {number} [vol=0.5] - The volume at which to play the audio (range: 0.0 to 1.0).
+ * @param {number} [volume=0.5] - The volume at which to play the audio (range: 0.0 to 1.0).
  */
-export function playAudio(file, volume = 0.5){
-  // This prevents error for autoplay being blocked by browser
-  // when the document is hidden (e.g., when the tab is not active).
-  if (document.hidden) {
-    return;
-  }
+export function playAudio(file, volume = 0.5) {
   var audio = document.createElement("audio");
   audio.src = file;
   audio.volume = volume;
