96936aa8423e1f9facf498e9647137a36b60399a
nicknickmak <nicknickmak@gmail.com>
2025-06-11 11:26:58 -0400

    fix: revamp experience screen

--- frontend/src/screens/ExperienceScreen/ExperienceScreen.js
+++ frontend/src/screens/ExperienceScreen/ExperienceScreen.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import SpecialButton from "../../components/SpecialButton";
-import "./card.css";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingBox from "../../components/LoadingBox";
 import MessageBox from "../../components/MessageBox";
 import { detailsExperience } from "../../actions/experienceActions";
+import "./card-details.css";
+import { useIsMobile } from "../../utils/useIsMobile";
 
 export default function ExperienceScreen(props) {
   const dispatch = useDispatch();
@@ -16,6 +17,8 @@ export default function ExperienceScreen(props) {
     dispatch(detailsExperience(experienceId));
   }, [dispatch, experienceId]);
 
+  const isMobile = useIsMobile();
+
   return (
     <div>
       {loading ? (
@@ -23,40 +26,32 @@ export default function ExperienceScreen(props) {
       ) : error ? (
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
-        <div className="notHome">
-          <div className="col-1 fadein">
-            <div className="row top">
-              <div className="col-1">
-                <div className="imageCard">
-                  <div className="imageCover">
-                    <h1>{experience.name}</h1>
-                    <p>{experience.date}</p>
-                  </div>
-                  <img
-                    className="large"
-                    src={experience.image}
-                    alt={experience.name}
-                  ></img>
-                </div>
+        <div className={`notHome ${isMobile ? "mobile" : ""}`}>
+          <div className="col-1">
+            <div className="imageCard">
+              <div className="imageCover">
+                <h1>{experience.name}</h1>
+                <p>{experience.date}</p>
               </div>
-              <div className="col-1">
-                <ul>
-                  <li>
-                    <h1>{experience.name}</h1>
-                  </li>
-                  <li>{experience.tech}</li>
-                  <br></br>
-                  <li>
-                    Description:
-                    <p>{experience.description}</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="row center">
-              <SpecialButton purpose={experience}></SpecialButton>
+              <img src={experience.image} alt={experience.name}></img>
             </div>
           </div>
+          <div className="">
+            <ul>
+              <li>
+                <h1>{experience.role}</h1>
+              </li>
+              <li>{experience.tech}</li>
+              <br></br>
+              <li>
+                Description:
+                <p>{experience.description}</p>
+              </li>
+            </ul>
+            <br></br>
+          </div>
+
+          <SpecialButton purpose={experience}></SpecialButton>
         </div>
       )}
     </div>
