8b0b4f791b424db3a8862c04d18a673829e341f8
nicknickmak <nicknickmak@gmail.com>
2025-06-11 17:19:30 -0400

    fix: remove backend and use config in front end temporarily

--- frontend/src/screens/ExperiencesScreen/ExperiencesScreen.js
+++ frontend/src/screens/ExperiencesScreen/ExperiencesScreen.js
@@ -4,16 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { listExperiences } from "../../actions/experienceActions";
 
 import "../carousel.css";
+import data from "../../data";
 
 export default function ExperiencesScreen() {
   function useFetchExperiences() {
-    const dispatch = useDispatch();
-    const experienceList = useSelector((state) => state.experienceList);
-    const { isLoading, error, experiences } = experienceList;
-    useEffect(() => {
-      dispatch(listExperiences());
-    }, [dispatch]);
+    // TODO setup Redux store and actions
+    // const dispatch = useDispatch();
+    // const experienceList = useSelector((state) => state.experienceList);
+    // const { isLoading, error, experiences } = experienceList;
+    // useEffect(() => {
+    //   dispatch(listExperiences());
+    // }, [dispatch]);
     // Sort experiences by number, ascending
+    const experiences = data.experiences; // Replace with Redux state when implemented
+    const isLoading = false; // Replace with Redux state when implemented
+    const error = null; // Replace with Redux state when implemented
+
     const sortedExperiences = experiences
       ? [...experiences].sort((a, b) => a.number - b.number)
       : [];
--- frontend/src/screens/ProjectsScreen.js
+++ frontend/src/screens/ProjectsScreen.js
@@ -4,16 +4,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { listProjects } from "../actions/projectActions";
 
 import "./carousel.css";
+import data from "../data";
 
 export default function ProjectsScreen() {
   function useFetchProjects() {
-    const dispatch = useDispatch();
-    const projectList = useSelector((state) => state.projectList);
-    const { isLoading, error, projects } = projectList;
-    useEffect(() => {
-      dispatch(listProjects());
-    }, [dispatch]);
+    // TODO setup Redux store and actions
+    // const dispatch = useDispatch();
+    // const projectList = useSelector((state) => state.projectList);
+    // const { isLoading, error, projects } = projectList;
+    // useEffect(() => {
+    //   dispatch(listProjects());
+    // }, [dispatch]);
     // Sort projects by number before returning
+
+    const projects = data.projects; // Replace with Redux state when implemented
+    const isLoading = false; // Replace with Redux state when implemented
+    const error = null; // Replace with Redux state when implemented
+
     const sortedProjects = projects
       ? [...projects].sort((a, b) => a.number - b.number)
       : [];
