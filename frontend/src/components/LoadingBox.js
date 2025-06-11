c13fdd6942829192c82a36ad7e020f1db57f705a
nicknickmak <nicknickmak@gmail.com>
2025-06-10 23:07:29 -0400

    fix: remove breaking sounds

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -1,15 +1,7 @@
-import loadingAudio from "../audio/loadingAnimation.mp3";
-import { playAudio } from "../utils/audioUtils";
-
 export default function LoadingBox() {
   return (
     <div className="row center">
-      <img
-        className="loading"
-        src="/images/animation.gif"
-        alt="LOADING"
-        onLoad={() => playAudio(loadingAudio)}
-      ></img>
+      <img className="loading" src="/images/animation.gif" alt="LOADING"></img>
     </div>
   );
 }
