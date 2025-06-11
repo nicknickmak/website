d88c68e776604e1e69b851ca147136cf1ae09969
nicknickmak <nicknickmak@gmail.com>
2025-06-11 18:06:37 -0400

    fix: remove api dependency in experience screen and project screen

--- frontend/src/screens/ExperienceScreen/ExperienceScreen.js
+++ frontend/src/screens/ExperienceScreen/ExperienceScreen.js
@@ -5,16 +5,26 @@ import LoadingBox from "../../components/LoadingBox";
 import MessageBox from "../../components/MessageBox";
 import { detailsExperience } from "../../actions/experienceActions";
 import { useIsMobile } from "../../utils/useIsMobile";
+import data from "../../data";
 
 export default function ExperienceScreen(props) {
-  const dispatch = useDispatch();
-  const experienceId = props.match.params.id;
-  const experienceDetails = useSelector((state) => state.experienceDetails);
-  const { loading, error, experience } = experienceDetails;
+  // TODO: Setup Redux store and actions
+  //   const dispatch = useDispatch();
+  //   const experienceId = props.match.params.id;
+  //   const experienceDetails = useSelector((state) => state.experienceDetails);
+  //   const { loading, error, experience } = experienceDetails;
 
-  useEffect(() => {
-    dispatch(detailsExperience(experienceId));
-  }, [dispatch, experienceId]);
+  //   useEffect(() => {
+  //     dispatch(detailsExperience(experienceId));
+  //   }, [dispatch, experienceId]);
+
+  const experienceId = props.match.params.id; // Replace with Redux state when implemented
+  const experience = data.experiences.find(
+    // Replace with Redux state when implemented
+    (experience) => experience._id === experienceId
+  );
+  const loading = false; // Replace with Redux state when implemented
+  const error = null; // Replace with Redux state when implemented
 
   const isMobile = useIsMobile();
 
--- frontend/src/screens/ProjectScreen.js
+++ frontend/src/screens/ProjectScreen.js
@@ -5,16 +5,26 @@ import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { detailsProject } from "../actions/projectActions";
 import { useIsMobile } from "../utils/useIsMobile";
+import data from "../data";
 
 export default function ProjectScreen(props) {
-  const dispatch = useDispatch();
-  const projectId = props.match.params.id;
-  const projectDetails = useSelector((state) => state.projectDetails);
-  const { loading, error, project } = projectDetails;
+  // TODO: Setup Redux store and actions
+  //   const dispatch = useDispatch();
+  //   const projectId = props.match.params.id;
+  //   const projectDetails = useSelector((state) => state.projectDetails);
+  //   const { loading, error, project } = projectDetails;
 
-  useEffect(() => {
-    dispatch(detailsProject(projectId));
-  }, [dispatch, projectId]);
+  //   useEffect(() => {
+  //     dispatch(detailsProject(projectId));
+  //   }, [dispatch, projectId]);
+
+  const projectId = props.match.params.id; // Replace with Redux state when implemented
+  const project = data.projects.find(
+    // Replace with Redux state when implemented
+    (project) => project._id === projectId
+  );
+  const loading = false; // Replace with Redux state when implemented
+  const error = null; // Replace with Redux state when implemented
 
   const isMobile = useIsMobile();
 
