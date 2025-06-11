b14c2a2fb17153ad2ee005ab409fc23669455e03
nicknickmak <nicknickmak@gmail.com>
2025-06-10 23:06:59 -0400

    refactor: format loadingBox file

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import loadingAudio from '../audio/loadingAnimation.mp3';
-import { playAudio } from '../utils/audioUtils';
-
+import loadingAudio from "../audio/loadingAnimation.mp3";
+import { playAudio } from "../utils/audioUtils";
 
 export default function LoadingBox() {
-    return (
-        <div className ="row center">
-            <img className="loading" src="/images/animation.gif" alt="LOADING"
-            onLoad={() => playAudio(loadingAudio)}
-            ></img>
-        </div>
-    )
+  return (
+    <div className="row center">
+      <img
+        className="loading"
+        src="/images/animation.gif"
+        alt="LOADING"
+        onLoad={() => playAudio(loadingAudio)}
+      ></img>
+    </div>
+  );
 }
